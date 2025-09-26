12195afc1d6e0de6d55c47ebc7a04d2eb5afbab5
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-09-26 14:55:33 +0200

    task 0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,9 @@
+import random
+import asyncio
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """Wait for some time"""
+    wait_time = random.random() * max_delay
+    await asyncio.sleep(wait_time)
+    return wait_time
